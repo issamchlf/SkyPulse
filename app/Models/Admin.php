52840ac1c628a02b536c1ecddcfea4e159a5620c
issamchlf <issamchellaf734@gmail.com>
2025-02-19 09:54:44 +0100

    Feat : Refactor Admin model to extend Authenticatable for improved authentication

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     use HasFactory, Notifiable;
     protected $guard = 'admin';
