5c9a6f94a0a0a5d5f57dae4dea1bd4522f31b6c8
issamchlf <issamchellaf734@gmail.com>
2025-02-24 23:41:37 +0100

    Feat : add new page route with admin access control middleware

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/page', function () {
+    return view('page');
+})->middleware( 'auth', 'is_admin:admin');
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware([ 'auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
