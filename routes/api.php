66c1f559efb5e205960d76c51c3eee93a449ffc4
issamchlf <issamchellaf734@gmail.com>
2025-02-26 08:35:29 +0100

    Feat : add API routes for Flight and Plane controllers with CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PlaneController;
+use App\Http\Controllers\Api\FlightController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::get('/flights', [FlightController::class, 'index'])->name('apiindex.flight');   
+route::get('/flights/{id}', [FlightController::class, 'show'])->name('apishow.flight');
+route::post('/flights', [FlightController::class, 'store'])->name('apistore.flight');
+route::put('/flights/{id}', [FlightController::class, 'update'])->name('apiupdate.flight');
+route::delete('/flights/{id}', [FlightController::class, 'destroy'])->name('apidelete.flight');
+
+Route::get('/planes', [PlaneController::class, 'index'])->name('apiindex.plane');
+Route::get('/planes/{id}', [PlaneController::class, 'show'])->name('apishow.plane');
+Route::post('/planes', [PlaneController::class, 'store'])->name('apistore.plane');
+Route::put('/planes/{id}', [PlaneController::class, 'update'])->name('apiupdate.plane');
+Route::delete('/planes/{id}', [PlaneController::class, 'destroy'])->name('apidelete.plane');
