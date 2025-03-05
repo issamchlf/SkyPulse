c07ea474904781afbf057820d339e0c4a1682668
issamchlf <issamchellaf734@gmail.com>
2025-03-05 13:36:23 +0100

    Refactor : rename method in Flight model and add picture attribute to Plane model

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -11,7 +11,7 @@ class Flight extends Model
     protected $table = 'flights';
     protected $fillable = ['airplane_id', 'flight_number', 'departure_airport', 'arrival_airport', 'departure_time', 'arrival_time', 'price', 'available_seats', 'status'];
 
-    public function Airplane()
+    public function plane()
     {
         return $this->belongsTo(plane::class);
     }
