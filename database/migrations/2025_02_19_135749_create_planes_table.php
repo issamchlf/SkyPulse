55e20527630e9b25afaee799704ea0501ef632f8
issamchlf <issamchellaf734@gmail.com>
2025-03-05 13:36:45 +0100

    Feat : add picture attribute to planes table migration

--- database/migrations/2025_02_19_135749_create_planes_table.php
+++ database/migrations/2025_02_19_135749_create_planes_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('type');
             $table->integer('max_seats');
+            $table->string('picture');
             $table->timestamps();
         });
     }
