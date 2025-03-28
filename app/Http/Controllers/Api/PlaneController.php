f17e69f35ad8a37bb03371bb3375943195e75587
issamchlf <issamchellaf734@gmail.com>
2025-03-28 13:00:43 +0100

    Feat: remove AdminController and unused methods from PlaneController to streamline codebase

--- app/Http/Controllers/Api/PlaneController.php
+++ app/Http/Controllers/Api/PlaneController.php
@@ -17,13 +17,6 @@ class PlaneController extends Controller
         return response()->json($planes);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -53,10 +46,7 @@ class PlaneController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
